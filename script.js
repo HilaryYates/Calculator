ff72be5882d08d346ac6165f5172869ee29a9319
hilaryyates <hilryyates@gmail.com>
2023-02-03 16:24:49 -0500

    styled calculator btns

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-const addButton = document.getElementById("add");
-const subtractButton = document.getElementById("subtract");
-const multiplyButton = document.getElementById("multiply");
-const divideButton = document.getElementById("divide");
+// const addButton = document.getElementById("add");
+// const subtractButton = document.getElementById("subtract");
+// const multiplyButton = document.getElementById("multiply");
+// const divideButton = document.getElementById("divide");
 
-const num1Input = document.getElementById("num1");
-const num2Input = document.getElementById("num2");
+// const num1Input = document.getElementById("num1");
+// const num2Input = document.getElementById("num2");
 
-const add = (num1, num2) => {
-  const resultDisplay = document.getElementById("result");
-  resultDisplay.innerHTML = num1 + num2;
-};
-const subtract = (num1, num2) => {
-  const resultDisplay = document.getElementById("result");
-  resultDisplay.innerHTML = num1 - num2;
-};
-const multiply = (num1, num2) => {
-  const resultDisplay = document.getElementById("result");
-  resultDisplay.innerHTML = num1 * num2;
-};
-const divide = (num1, num2) => {
-  const resultDisplay = document.getElementById("result");
-  resultDisplay.innerHTML = num1 / num2;
-};
+// const add = (num1, num2) => {
+//   const resultDisplay = document.getElementById("result");
+//   resultDisplay.innerHTML = num1 + num2;
+// };
+// const subtract = (num1, num2) => {
+//   const resultDisplay = document.getElementById("result");
+//   resultDisplay.innerHTML = num1 - num2;
+// };
+// const multiply = (num1, num2) => {
+//   const resultDisplay = document.getElementById("result");
+//   resultDisplay.innerHTML = num1 * num2;
+// };
+// const divide = (num1, num2) => {
+//   const resultDisplay = document.getElementById("result");
+//   resultDisplay.innerHTML = num1 / num2;
+// };
 
-addButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  add(+num1Input.value, +num2Input.value);
-});
+// addButton.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   add(+num1Input.value, +num2Input.value);
+// });
 
-subtractButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  subtract(+num1Input.value, +num2Input.value);
-});
+// subtractButton.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   subtract(+num1Input.value, +num2Input.value);
+// });
 
-multiplyButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  multiply(+num1Input.value, +num2Input.value);
-});
+// multiplyButton.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   multiply(+num1Input.value, +num2Input.value);
+// });
 
-divideButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  divide(+num1Input.value, +num2Input.value);
-});
+// divideButton.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   divide(+num1Input.value, +num2Input.value);
+// });
